0e033b428515a957f841e5645acb933af43c4d82
David <daviddeoliveirasantos@gmail.com>
2021-06-05 03:35:47 -0300

    Adicionado o portal de agendamentos.

---
+++ scheduler/SOA/sc/Comunicator.java
@@ -0,0 +1,66 @@
+package sc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Comunicator {
+    
+    
+    public void doTask(String appName,String arg1) {
+
+        try {
+
+            URL url = new URL("http://localhost:7003/soa-infra/resources/default/scheduler/SchedulerService");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            String input = 
+            "{\n" + 
+            "    \"app-name\" : \""+appName+"\",\n" + 
+            "    \"arg1\" : \""+arg1+"\"\n" + 
+            "}";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_NO_CONTENT) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+    
+}
--- scheduler/SOA/sc/Scheduler.java
+++ scheduler/SOA/sc/Scheduler.java
@@ -1,83 +1,42 @@
 package sc;
 
-import java.io.BufferedReader;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-
-import java.net.URL;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableScheduling
-public class Scheduler {
+public class Scheduler extends Comunicator {
 
-    public Scheduler() {
-        super();
-    }
+/** 
+ *  Sessão: Portal de agendamentos
+ * 
+ * **/
+
+    /** Envio de relatório as 9:05 **/
+    @Scheduled(cron = "0 5 9 ? * *")
+    public void reportPortalAgendamentosPeriod1() {doTask("PortalReport","period1");}
     
-    @Scheduled(cron = "*/5 * * * * *")
-    public void scheculerapp1() {doTask("Portal");}
+    /** Envio de relatório as 12:05 **/
+    @Scheduled(cron = "0 5 12 ? * *")
+    public void reportPortalAgendamentosPeriod2() {doTask("PortalReport","period2");}
     
-    @Scheduled(cron = "*/5 * * * * *")
-    public void scheculerapp2() {doTask("appName2");}
+    /** Envio de relatório as 15:05 **/
+    @Scheduled(cron = "0 5 15 ? * *")
+    public void reportPortalAgendamentosPeriod3() {doTask("PortalReport","period3");}
     
-    @Scheduled(cron = "*/5 * * * * *")
-    public void scheculerapp3() {doTask("appName3");}
+    /** Envio de relatório as 18:05 **/
+    @Scheduled(cron = "0 5 18 ? * *")
+    public void reportPortalAgendamentosPeriod4() {doTask("PortalReport","period4");}
 
+    /** Envio de relatório de teste **/
+    //@Scheduled(cron = "0 * * * * *")
+    //public void reportPortalAgendamentosTeste() {doTask("PortalReport","period1");}
+/** 
+ * 
+ * Sessão: Portal de agendamentos
+ * 
+ * **/
 
-    public void doTask(String appName) {
-
-        try {
-
-            URL url = new URL("http://localhost:7003/soa-infra/resources/default/scheduler/SchedulerService");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-
-            String input = "{\"app-name\":\"" + appName + "\"}";
-
-            OutputStream os = conn.getOutputStream();
-            os.write(input.getBytes());
-            os.flush();
-
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_NO_CONTENT) {
-                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
-
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
-
-    }
 
 }
